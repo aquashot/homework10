a0488b488cd3073dd341d8bc97f64f30953efe22
Илон <vlasovvdm@gmail.com>
2023-11-08 18:08:22 +0400

    !

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        var firstName = "ivan";
+        var middleName = "ivanov";
+        var lastname = "ivanovich";
+        var fullname = firstName + " " + middleName + " " + lastname + " ";
+        System.out.println(fullname);
+
+        System.out.println("задача 2");
+        var firstname = "ivan";
+        var middlename = "ivanov";
+        var lastName = "ivanovich";
+        var fullName = firstname + " " + middlename + " " + lastName + " ";
+        fullName = fullName.toUpperCase();
+        System.out.println(fullname.toUpperCase());
+        System.out.println("Задача 3");
+        var fIrstname = "Семён";
+        var mIddlename = "ivanov";
+        var lAstName = "Семёнович";
+        var fUllName = fIrstname + " " + mIddlename + " " + lAstName + " ";
+        fUllName=fUllName.replace("ё","е");
+                System.out.println(fUllName);
+
     }
 }
+
